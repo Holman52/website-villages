a0611e625c71d16ec0c76aaec27feec5d7ea370c
Ivan Holm <vinokur4515@gmail.com>
2024-09-12 00:37:02 +0300

    адаптив страницы грачи под устройства от 769px

--- JS/grach.js
+++ JS/grach.js
@@ -7,13 +7,13 @@ function init(){
     let placemark = new ymaps.Placemark([56.668155, 44.309725], {}, {
 		});
 
-    // map.controls.remove('geolocationControl'); // удаляем геолокацию
-    // map.controls.remove('searchControl'); // удаляем поиск
-    // map.controls.remove('trafficControl'); // удаляем контроль трафика
-    // map.controls.remove('typeSelector'); // удаляем тип
-    // map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-    // map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-    // map.controls.remove('rulerControl'); // удаляем контрол правил
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
     map.geoObjects.add(placemark);
 
 }
